b6ee0e891f063261a489f5a56928756cc5c2829d
Dong Lu <144203360+dl32958@users.noreply.github.com>
2024-12-13 14:36:22 -0800

    add review model

---
+++ be-yelpcamp/models/Review.js
@@ -0,0 +1,9 @@
+import mongoose from 'mongoose';
+const Schema = mongoose.Schema;
+
+const ReviewSchema = new Schema({
+    body: String,
+    rating: Number,
+});
+
+export default mongoose.model('Review', ReviewSchema);
