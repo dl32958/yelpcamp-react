fee2b17c049d88171f923251f7fb6d39910d1d1d
Dong Lu <144203360+dl32958@users.noreply.github.com>
2024-12-16 12:35:54 -0800

    add authentication

--- be-yelpcamp/app.js
+++ be-yelpcamp/app.js
@@ -2,8 +2,9 @@ import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import ExpressError from './utils/ExpressError.js';
-import campgroundRoutes from './routes/campground.js';
+import campgroundRoutes from './routes/campgrounds.js';
 import reviewRoutes from './routes/reviews.js';
+import userRoutes from './routes/users.js';
 import session from 'express-session';
 
 const app = express();
@@ -12,30 +13,34 @@ const corsOptions = {
     credentials: true,
 };
 
+// mongoDB
 mongoose.connect('mongodb://localhost:27017/yelpcamp');
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'database connection error:'));
 db.once('open', () => {
     console.log('database connected');
 });
+// middleware
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const sessionConfig = {
-    secret: 'thisisnotagoodsecret',
-    resave: false,
-    saveUninitialized: true,
-    cookie: {
-        httpOnly: true,
-        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
-        maxAge: 1000 * 60 * 60 * 24 * 7,
-    }
-};
-app.use(session(sessionConfig));
+// const sessionConfig = {
+//     secret: 'thisisnotagoodsecret',
+//     resave: false,
+//     saveUninitialized: true,
+//     cookie: {
+//         httpOnly: true,
+//         expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+//         maxAge: 1000 * 60 * 60 * 24 * 7,
+//     }
+// };
+// app.use(session(sessionConfig));
 
+// routes
 app.use('/campgrounds', campgroundRoutes);
 app.use('/campgrounds/:id/reviews', reviewRoutes);
+app.use('/users', userRoutes);
 
 
 app.get('/', (req, res) => {
---
+++ be-yelpcamp/routes/users.js
@@ -0,0 +1,78 @@
+import express from 'express';
+import User from '../models/User.js';
+import CatchAsync from '../utils/CatchAsync.js';
+import ExpressError from '../utils/ExpressError.js';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+
+const router = express.Router();
+const JWT_SECRETE = process.env.JWT_SECRETE || "thisisnotagoodsecrete";
+
+router.post('/register', CatchAsync(async (req, res) => {
+    try {
+        const { email, password } = req.body.user;
+        console.log(email, password);
+        const existingUser = await User.findOne({ email: email });
+        if (existingUser) {
+            throw new ExpressError('User already exists!', 409);
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const newUser = new User({ email, password: hashedPassword });
+        await newUser.save();
+
+        const token = jwt.sign({ id: newUser._id, email: newUser.email }, JWT_SECRETE, { expiresIn: '1h' });
+        console.log(token);
+        res.status(200).json({
+            message: 'Login successful!',
+            token,
+            user: {
+                id: newUser._id,
+                email: newUser.email
+            }
+        })
+    } catch (e) {
+        throw new ExpressError(e.message, 409);
+    }
+}));
+
+router.post('/login', CatchAsync(async (req, res) => {
+    console.log(req.body);
+    const { email, password } = req.body.user;
+    const user = await User.findOne({ email: email });
+    console.log(user);
+    if (!user) {
+        throw new ExpressError('Invalid username or password', 401);
+    }
+    const isMatch = await bcrypt.compare(password, user.password);
+    console.log(isMatch);
+    if (!isMatch) {
+        throw new ExpressError('Invalid username or password', 401);
+    }
+
+    const token = jwt.sign({ id: user._id, email: user.email }, JWT_SECRETE, { expiresIn: '1h' });
+    res.status(200).json({
+        message: 'Login successful!',
+        token,
+        user: {
+            id: user._id,
+            email: user.email
+        }
+    })
+}));
+
+router.get('/get-user', CatchAsync(async (req, res) => {
+    const token = req.headers.authorization.split(' ')[1];
+    if (!token) {
+        return res.status(401).send('Unauthorized');
+    }
+    try {
+        const decoded = jwt.verify(token, JWT_SECRETE);
+        console.log('decoded',decoded);
+        res.status(200).send(decoded);
+    } catch (e) {
+        res.status(401).send('Unauthorized');
+    }
+}));
+
+export default router;
